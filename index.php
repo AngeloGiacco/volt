5de9630475965d8ba1e22cf9059710125151f4d9
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-05-12 23:26:14 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html>
+    <head>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+
+        <title>Volt</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,600" rel="stylesheet">
+        <link href='https://fonts.googleapis.com/css?family=Merriweather:300,700' rel='stylesheet' type='text/css'>
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href="style.css" rel="stylesheet">
+
+    </head>
+    <body>
+
+        <header>
+            <div class="container">
+                <h1 class="logo"><i class="fa fa-magnet"></i>Volt</h1>
+                <i class="fa fa-bars toggle-menu"></i>
+                <nav>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        <section class="splash">
+            <div class="container">
+                <h1>Volt</h1>
+                <a class="btn" href="#help" id="black">Find out about our charity<i class="fa fa-angle-down"></i></a>
+            </div>
+        </section>
+
+        <section id = "help" class="feature greybg">
+            <div class="container">
+                <img src="agri.jpg" alt="Indonesia Map">
+                <div>
+                    <h1>Volt</h1>
+                    <p>Volt is a charity founded by me, Angelo Giacco. I was
+                       motivated to found this charity after my friend told
+                       me of a low-cost technology he was working on to make
+                       agrivoltaic farming viable for Indonesian farmers.
+                       They are some of the poorest people in the world,
+                       earning around $2 a day. I plan to use funds raised for
+                       the charity to subsidise farmers' purchase of the tech.
+                       Agrivoltaic farming is when a farmer can produce crops
+                       whilst having solar panels on the land.
+                       Want to find out the <a href = "#benefits">benefits</a>
+                       of this system? Scroll down!</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="overview3 overview" id = "benefits">
+            <div class="container">
+              <h1>Benefits of using agrivoltaics</h1>
+                <div>
+                    <i class="fa fa-fire"></i>
+                    <h2>Help lift Indonesian farmers out of poverty</h2>
+                    <p>By both producing crops and solar energy, the farmers
+                       can earn more money and this will help alleviate the
+                       poverty in their regions.
+                    </p>
+                </div>
+                <div>
+                    <i class="fa fa-mobile"></i>
+                    <h2>Increase development</h2>
+                    <p>Agrivoltaic farming will help to introduce electricity to
+                       some areas of Indonesia for the first time which will
+                       help to sustainably develop these areas.</p>
+                </div>
+                <div>
+                    <i class="fa fa-users"></i>
+                    <h2>Save the planet!</h2>
+                    <p>Indonesia is one of the most polluting countries on Earth,
+                       as any Malaysian will tell you, so creating solar energy will go some way
+                       towards weaning Indonesia off non-renewable enery.</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="contact">
+            <div class="container">
+                <h1>Get in touch</h1>
+                <h2>Have any thoughts or want to donate?<h2>
+                <h3>Either way, feel free to email <a href="mailto:giacco.angelo.s@gmail.com">me</a></h3>
+            </div>
+        </section>
+
+        <footer>
+            <div class="container">
+                <div class="copyright">
+                    <p>Implemented by Angelo Giacco. <a href="http://twitter.com/giaccoangelo" target="_blank">Follow me on Twitter</a>.</p>
+                </div>
+                <div class="social">
+                    Share This:
+                    <a href="https://www.facebook.com/sharer/sharer.php?u=voltindo.herokuapp.com" target="_blank"><i class="fa fa-facebook"></i></a>
+                    <a href="https://www.reddit.com/submit?url=voltindo.herokuapp.com" target="_blank"><i class="fa fa-reddit"></i></a>
+                    <a href="https://www.linkedin.com/shareArticle?mini=true&url=voltindo.herokuapp.com" target="_blank"><i class="fa fa-linkedin"></i></a>
+                </div>
+            </div>
+        </footer>
+    </body>
